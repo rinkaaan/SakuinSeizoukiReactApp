7a720a29e9ae74584480db18cf7acb063842d6c5
Lincoln Nguyen <nguylinc@amazon.com>
2023-12-11 19:08:04 -0800

    Add icon, cloudscape components

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './Home.css'
 import { useLoaderData } from 'react-router-dom'
 import { TimeService } from '../openapi-client'
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import '@cloudscape-design/global-styles/index.css'
 import App from './App.tsx'
 import { OpenAPI } from '../openapi-client'
 
